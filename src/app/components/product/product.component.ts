8c05b73cef3e2751f9fcb57006c7cf3f8c7f68d4
elifclk444@gmail.com <elifclk444@gmail.com>
2021-03-19 00:47:40 +0300

    .

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,8 +14,9 @@ export class ProductComponent implements OnInit {
 
   products:Product[] = [];
   dataLoaded=false;
+  filterText="";
   
-  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute) { }
+  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute, private toastrService:ToastrService,private cartService:CartService) { }
 
   ngOnInit(): void {
     
@@ -40,6 +43,19 @@ export class ProductComponent implements OnInit {
       this.products=response.data
       this.dataLoaded=true;
     })
+
+  }
+
+  addToCart(product:Product){
+    if(product.productId===1){
+      this.toastrService.error("Hata","Sepete eklenemez")
+    }else{
+      this.toastrService.success("Sepete eklendi",product.productName)
+      this.cartService.addToCart(product);
+    }
+
+    
+
 
   }
 
